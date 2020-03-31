3ce4d02ee9d424acb57b889414b60bfc273999f0
liupeng05 <liupeng05@kuaishou.com>
2020-03-31 21:14:25 +0800

    coco data error

--- efficientdet/scripts/train.sh
+++ efficientdet/scripts/train.sh
@@ -8,7 +8,7 @@ mpirun -np $num_gpus -H localhost:$num_gpus \
 --allow-run-as-root -bind-to none -map-by slot -x NCCL_DEBUG=INFO \
 -x LD_LIBRARY_PATH -x PATH -mca pml ob1 -mca btl ^openib \
 --mca btl_smcuda_use_cuda_ipc 0 \
-python main.py --training_file_pattern=/share/liupeng/data/cv/coco/tfrecord/coco_train* \
+python main.py --training_file_pattern=/share/liupeng/data/cv/coco/coco_tfrecord/train* \
     --model_name=$MODEL \
     --model_dir=/tmp/$MODEL \
     --hparams="use_bfloat16=false" \
